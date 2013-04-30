8553c229cb3474bc4ad41178c67178437ed9290f
Panos Kittenis <panos@zeebox.com>
2013-04-30 11:45:45 +0100

    Added recursive versions of p1 and p2. Changed fibonacci to tail recursion in p2 and using match

--- p1.ml
+++ p1.ml
@@ -1,6 +1,22 @@
+open List;;
+
 let x = ref 0;;
 for i = 0 to 999 do
  if i mod 5 == 0 || i mod 3 == 0 then x := i + !x;
 done;
 !x;;
-Printf.printf "%d\n" !x;;
+Printf.printf "Loop version: %d\n" !x;;
+
+let sum = List.fold_left (+) 0;;
+
+let p1 max =
+  let rec f n =
+    if n <= max && ((n mod 5 == 0) || (n mod 3 == 0)) then
+      n :: f (n+1)
+    else if n <= max then
+      f (n+1)
+    else
+      []
+  in f 0;;
+
+Printf.printf "Recursive version: %d\n" (sum(p1 999));;
--- p2.ml
+++ p2.ml
@@ -1,13 +1,21 @@
 open List;;
 
-let rec fib = function
-  | 0 -> 0
-  | 1 -> 1
-  | n -> fib (n-1) + fib (n-2);;
+(* Plain recursion, slow due to 2* recursive calls to fib *)
+(* let rec fib = function *)
+(*   | 0 -> 0 *)
+(*   | 1 -> 1 *)
+(*   | n -> fib (n-1) + fib (n-2);; *)
+
+(* tail recursion, faster *)
+let fib n =
+  let rec f n x y =
+    if n <= 0 then y
+    else f (n-1) (x+y) x in
+  f n 1 0;;
 
 let sum = List.fold_left ( + ) 0;;
 
-let even i = 
+let even i =
   i mod 2 == 0;;
 
 let x = ref [];;
@@ -19,4 +27,19 @@ while not (!m >= 4000000) do
   i := !i + 1;
 done;
 
-Printf.printf "%d\n" (sum !x);;
+Printf.printf "Loop version: %d\n" (sum !x);;
+
+let p2 max =
+  let rec f n =
+    let m = fib n in
+    match m with
+    | m when m < max && m mod 2 == 0 -> m :: f (n+1)
+    | m when m < max -> f (n+1)
+    | _ -> []
+  in f 0;;
+  (*   if m < max && m mod 2 == 0 then m :: f (n+1) *)
+  (*   else if m < max then f (n+1) *)
+  (*   else [] *)
+  (* in f 0;; *)
+
+Printf.printf "Recursive version: %d\n" (sum (p2 4000000));;
